c145415900894935280c70757f7b7ed43eb0f5ed
Christobal <geekarist@users.noreply.github.com>
2019-06-12 09:43:45 +0200

    Create TokenProvider

---
+++ app/src/main/java/me/cpele/runr/ui/main/TokenProvider.kt
@@ -0,0 +1,7 @@
+package me.cpele.runr.ui.main
+
+class TokenProvider {
+    fun get(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- app/src/main/java/me/cpele/runr/ui/main/TrackRepository.kt
+++ app/src/main/java/me/cpele/runr/ui/main/TrackRepository.kt
@@ -2,9 +2,11 @@ package me.cpele.runr.ui.main
 
 import android.util.Log
 
-class TrackRepository {
+class TrackRepository(val tokenProvider: TokenProvider) {
     fun findByPace(pace: Int): List<TrackBo> {
+        val token = tokenProvider.get()
         Log.d(javaClass.simpleName, "Find tracks with pace $pace")
+        Log.d(javaClass.simpleName, "Spotify auth token: $token")
         return listOf()
     }
 }
