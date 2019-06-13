223fa0dac60d2b7e261646d6af1f41d3f9c16b5f
Christobal <geekarist@users.noreply.github.com>
2019-06-13 09:31:53 +0200

    Implement LoginActivity

--- app/src/main/java/me/cpele/runr/ui/main/LoginActivity.kt
+++ app/src/main/java/me/cpele/runr/ui/main/LoginActivity.kt
@@ -2,23 +2,49 @@ package me.cpele.runr.ui.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.spotify.sdk.android.authentication.AuthenticationClient
+import com.spotify.sdk.android.authentication.AuthenticationRequest
+import com.spotify.sdk.android.authentication.AuthenticationResponse
+import me.cpele.runr.BuildConfig
 import me.cpele.runr.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        Handler().postDelayed({
-            LocalBroadcastManager.getInstance(this).sendBroadcast(
-                Intent("ACTION_SPOTIFY_LOGIN").putExtra(
-                    "EXTRA_SPOTIFY_TOKEN",
-                    "TODO"
-                )
+        val clientId = BuildConfig.SPOTIFY_CLIENT_ID
+        val redirectUri = "me.cpele.runr://spotify-redirect"
+        val scopes = arrayOf("user-read-private", "app-remote-control")
+        val request = AuthenticationRequest.Builder(
+            clientId,
+            AuthenticationResponse.Type.TOKEN,
+            redirectUri
+        ).setScopes(scopes).build()
+        AuthenticationClient.openLoginActivity(this, 42, request)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 42) {
+            val authResponse = AuthenticationClient.getResponse(resultCode, data)
+            when (authResponse.type) {
+                AuthenticationResponse.Type.TOKEN -> sendTokenThenFinish(authResponse.accessToken)
+                AuthenticationResponse.Type.ERROR -> TODO()
+                else -> TODO()
+            }
+        }
+    }
+
+    private fun sendTokenThenFinish(token: String) {
+        LocalBroadcastManager.getInstance(this).sendBroadcast(
+            Intent("ACTION_SPOTIFY_LOGIN").putExtra(
+                "EXTRA_SPOTIFY_TOKEN",
+                token
             )
-            finish()
-        }, 2000)
+        )
+        finish()
     }
 }
